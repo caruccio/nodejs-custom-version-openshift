58853376971a3ba76930f602205c3ebe8945e7d6
Mateus Caruccio <mateus@caruccio.com>
2014-08-13 10:13:53 -0300

    Add websocket endpoints

--- meteor-server.js
+++ meteor-server.js
@@ -6,6 +6,9 @@ process.env.ROOT_URL = 'http://' + (process.env.OPENSHIFT_APP_DNS || "localhost"
 process.env.MONGO_URL = (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) || "mongodb://localhost:27017/meteor";
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+// WebSocket endpoints
+process.env.DDP_DEFAULT_CONNECTION_URL='http://' + process.env.OPENSHIFT_APP_DNS + ':8000';
+//process.env.DDP_DEFAULT_CONNECTION_URL='https://' + process.env.OPENSHIFT_APP_DNS + ':8443';
 
 // Show connection details on startup
 console.log("MONGO_URL IS: " + process.env.MONGO_URL);
